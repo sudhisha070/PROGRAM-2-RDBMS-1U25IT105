e427fa069b1740468f18380485d86318d5dcacc6
SUDHISHA M <sudhisha070@gmail.com>
2026-08-28 12:19:01 +0530

    Program 2

--- solution.sql
+++ solution.sql
@@ -1,1 +1,10 @@
-
+CREATE DATABASE Class;
+USE Class;
+CREATE TABLE Student(
+  StudentID INT(5) PRIMARY KEY,
+  StudentName VARCHAR(20),
+  DOB INT(10),
+  Gender VARCHAR(10),
+  DepartmentID INT(10)
+);
+DESC Department;
